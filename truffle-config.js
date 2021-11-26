ad1ee8f6df7f5e406f86e1e21e6e140d51a2ddd4
Nico Rakover <nrakover@gmail.com>
2021-11-26 08:55:46 -0800

    rename 'development' network to 'local'

--- truffle-config.js
+++ truffle-config.js
@@ -33,18 +33,13 @@ module.exports = {
    */
 
   networks: {
-    test: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    development: {
+    local: {
       host: "host.docker.internal",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
