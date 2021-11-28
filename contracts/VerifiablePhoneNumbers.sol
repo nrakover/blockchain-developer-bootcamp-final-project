bfc8a037ef13f78ad960e5dcc29a463e8cedf919
Nico Rakover <nrakover@gmail.com>
2021-11-28 07:41:45 -0800

    factor sampling logic into RandomUtil.sol

--- contracts/VerifiablePhoneNumbers.sol
+++ contracts/VerifiablePhoneNumbers.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+import "./RandomUtil.sol";
+
 contract VerifiablePhoneNumbers {
     /* Types */
     struct PhoneNumber {
@@ -72,7 +74,6 @@ contract VerifiablePhoneNumbers {
 
     /* Constants */
     uint8 constant MAX_SELECTED_VERIFIERS = 8; // This is due to how we generate a random sampling of verifiers
-    uint8 constant MAX_SAMPLING_RETRIES = 10;
 
     /* State */
     address public owner;
@@ -363,60 +364,6 @@ contract VerifiablePhoneNumbers {
         return false;
     }
 
-    function _getRandomSeed(uint256 verificationRequestId)
-        private
-        view
-        returns (uint256)
-    {
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(
-                        block.number,
-                        msg.sender,
-                        verificationRequestId
-                    )
-                )
-            );
-    }
-
-    function _generatePseudoRandomNum(uint256 seed, uint8 i)
-        private
-        pure
-        returns (uint256)
-    {
-        return uint256(keccak256(abi.encode(seed + i)));
-    }
-
-    function _sampleUnique(
-        uint32 n,
-        uint8 k,
-        uint256 seed
-    ) private pure returns (uint32[] memory) {
-        uint32[] memory samples = new uint32[](k);
-        uint8 i = 0;
-        uint8 retries = 0;
-        while (i < k) {
-            uint256 rand = _generatePseudoRandomNum(seed, i + retries);
-            uint32 s = uint32(rand % n);
-            bool unique = true;
-            for (uint8 j = 0; j < i; j++) {
-                if (s == samples[j]) {
-                    unique = false;
-                    break;
-                }
-            }
-            if (unique) {
-                samples[i] = s;
-                i++;
-            } else {
-                retries++;
-                assert(retries < MAX_SAMPLING_RETRIES);
-            }
-        }
-        return samples;
-    }
-
     function _selectAndAssignRandomVerifiers(uint256 verificationRequestId)
         private
         returns (address[] storage)
@@ -425,10 +372,10 @@ contract VerifiablePhoneNumbers {
         address[] storage selectedVerifiers = verificationRequests[
             verificationRequestId
         ].selectedVerifiers;
-        uint32[] memory selectedVerifierIndices = _sampleUnique(
+        uint32[] memory selectedVerifierIndices = RandomUtil.sampleUnique(
             numVerifiers,
             numVerifiersPerRequest,
-            _getRandomSeed(verificationRequestId)
+            RandomUtil.getRandomSeed(verificationRequestId)
         );
         for (uint8 i = 0; i < numVerifiersPerRequest; i++) {
             uint32 verifierIndex = selectedVerifierIndices[i];
